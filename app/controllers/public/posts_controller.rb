02bec278044d6bba1f76c007bbe60b28d0f74051
美坂朱里 <shuri1201@icloud.com>
2020-04-05 12:49:59 +0000

    s

--- app/controllers/public/posts_controller.rb
+++ app/controllers/public/posts_controller.rb
@@ -23,7 +23,7 @@ class Public::PostsController < Public::ApplicationController
   def update
     @post = Post.find(params[:id])
     @post.update(post_params)
-    redirect_to public_post_path(@post.id), flash:{success: "You have updated user successfully."}
+    redirect_to public_post_path(@post.id)
   end
 
   def create
